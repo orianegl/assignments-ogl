693b0b6afe80e5876093fabb3f895779f142e2ac
orianegl <zsr3ev@virginia.edu>
2025-01-23 15:49:38 -0500

    python homework

---
+++ week05/week05.py
@@ -0,0 +1,7 @@
+word = input("Enter word: ")
+# Pig latin code goes here
+print(pig_latin)
+
+
+greetings = ""
+print (greeting)
---
+++ week06.py
@@ -0,0 +1,22 @@
+#below is same prompt but phrased differently
+
+def pig_latin(word):
+  prefix = word[1:]
+  suffix = word[0]
+  return prefix + suffix + "ay"
+
+print("Hazel"[1:]+"Hazel"[0]+"ay")
+print("is"[1:]+"is"[0]+"ay")
+print("a"[1:]+"a"[0]+"ay")
+print("good"[1:]+"good"[0]+"ay")
+print("dog"[1:]+"dog"[0]+"ay")
+
+def pig_latin(word):
+  return word[1:]+word[0].lower()+"ay"
+
+def repeat (word, number):
+  return (word+" ")*number
+
+input = "praxis codelab code lab"
+l = input.split(" ")    
+print(repeat(pig_latin(l[0]),10) + " ".join(l[1:]))
